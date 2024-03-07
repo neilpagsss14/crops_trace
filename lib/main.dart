49f04fa6fee7b4d3ee0b59542bf68b10cb66d881
neilpagsss14 <neilpagara50@gmail.com>
2024-03-08 07:32:54 +0800

    latest wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:crop_traceability/firebase_options.dart';
 import 'package:crop_traceability/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
